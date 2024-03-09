4afae5ca3dfdc987fa31c36925d1f1e8020c04c6
b-hairston <bph484@gmail.com>
2024-03-09 09:57:44 -0600

    added the bean search command

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import requests
+import random
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+SEARCH_ENGINE_ID = os.getenv('SEARCH_ENGINE_ID')
 
 intents = discord.Intents.default()
 intents.message_content = True  # Enable intents for receiving message content
@@ -14,7 +18,31 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.command()  # Use the bot.command() decorator to register a command
 async def hello(ctx):
     await ctx.send(f'Hello {ctx.author.display_name}.')
+@bot.command()
+async def image(ctx, *, search_query):
+    search_url = "https://www.googleapis.com/customsearch/v1"
+    params = {
+        "key": GOOGLE_API_KEY,
+        "cx": SEARCH_ENGINE_ID,
+        "q": str(search_query),  # Ensuring the query is treated as a phrase
+        "searchType": "image",
+        "imgColorType": "color",  # Only fetch color image
+        "num": 10   }
+    
+    response = requests.get(search_url, params=params).json()
+    
+    print(response)
+    
+    if 'items' in response:
+        random_image_url = random.choice(response['items'])['link']
+        await ctx.send(random_image_url)
+    else:
 
+        await ctx.send(f"I couldn't find any {search_query}. :sob:")
+        
+    
+        
+    
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
